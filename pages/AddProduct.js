47af6d381747a67df0b6d29a9d2883deae8ebc7f
Pawel <kielczak1999@gmail.com>
2021-12-29 14:09:02 +0100

    commit

---
+++ pages/AddProduct.js
@@ -0,0 +1,160 @@
+// React Navigate Drawer with Bottom Tab
+// https://aboutreact.com/bottom-tab-view-inside-navigation-drawer/
+
+import * as React  from 'react';
+import {Button, Image, View, Text,StyleSheet, TextInput, TouchableOpacity, SafeAreaView} from 'react-native';
+
+function AddProduct({ navigation }) {
+  return (
+    <SafeAreaView style={{flex: 1}} name="AddProduct">
+      <View style={styles.sectionStyle,{flex: 1, padding: 16, alignItems: 'center',
+            justifyContent: 'center',}}>
+      
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'bottom',
+              marginBottom: 16,
+            }}>
+            Dodaj produkt
+          </Text>
+
+    <View style={styles.sectionStyle}>
+      <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',
+            }}
+            style={styles.imageStyle}
+          />    
+      <TextInput
+        placeholder="Nazwa"
+    />
+    </View>
+
+    <View style={styles.sectionStyle}>
+      <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',
+            }}
+            style={styles.imageStyle}
+          />
+      <TextInput
+        placeholder="Cena"
+    />
+    </View>
+
+
+    <View style={styles.sectionStyle}>
+      <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',
+            }}
+            style={styles.imageStyle}
+          />
+    <TextInput
+        placeholder="Opis"
+    />
+    </View>
+
+    <View style={styles.sectionStyle}>
+      <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',
+            }}
+            style={styles.imageStyle}
+          />
+      <TextInput
+        placeholder="Lokalizacja"
+    />
+    </View>
+
+    <View style={styles.sectionStyle}>
+      <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/input_phone.png',
+            }}
+            style={styles.imageStyle}
+          />
+      <TextInput
+        placeholder="Nr telefonu do kontaktu"
+    />
+    </View>
+
+          <Button
+            //onPress={() => navigation.navigate('Home')}
+            title="Dodaj"
+            style={styles.butony}
+          />
+        
+        </View>
+    
+    </SafeAreaView>
+    
+  );
+};
+
+
+const styles = StyleSheet.create({
+  tlo: {
+      backgroundColor: 'rgb(247, 247, 247)',
+      fontSize: 15,
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      navbarBackgroundColor: '#2c3e50',
+      statusBarColor: '#233240'
+    },
+
+    sectionStyle: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      borderWidth: 0.5,
+      borderColor: '#000',
+      height: 40,
+      borderRadius: 5,
+      margin: 10,
+    },
+
+    inputy: {
+      fontSize: 18, 
+      textAlign: 'center', 
+      color: 'grey',
+      mx: 6,
+      borderWidth: 1,
+      borderColor: 'rgb(111, 121, 247)',
+      padding: 5,
+      marginTop: 10,
+      marginBottom: 16,
+      base: "75%",
+      md: "45%",
+      underlineColorAndroid:"transparent",
+    },
+
+    butony: {
+      marginTop: 12,
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'rgb(96, 112, 128)',
+      color: 'rgb(50, 168, 82)',
+    },
+
+    imageStyle: {
+      padding: 10,
+      margin: 5,
+      height: 25,
+      width: 25,
+      resizeMode: 'stretch',
+      alignItems: 'center',
+    },
+});
+
+export default AddProduct;
