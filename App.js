8e9b9fb7cc27503e125aba804955c063dfc81d2b
przenowako <62574960+przenowako@users.noreply.github.com>
2022-01-20 00:42:12 +0100

    logowanie oraz rejestracja

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NativeBaseProvider, Text, Box, useTheme } from 'native-base';
 import React, { Component } from 'react';
-import { BackHandler } from 'react-native';
+import { BackHandler, AppRegistry } from 'react-native';
 import { Root } from 'native-base';
 import AppContainer from './pages/Login';
 import Login from './pages/Login';
@@ -13,25 +13,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return(
-<NavigationContainer independent={true}>
-  <Stack.Navigator>
-    <Stack.Screen name="Login" component={Login} options={{
-          headerTintColor:"white",
-          headerStyle:{backgroundColor: "rgb(96, 112, 128)"},
-          }}/>
-    <Stack.Screen name="Home" component={HomeScreen} options={{
-          headerStyle:{backgroundColor: "rgb(96, 112, 128)"},
-          headerTintColor:"white",
-          }}/>
-    <Stack.Screen name="Rejestracja" component={Rejestracja} options={{
-        headerStyle: { backgroundColor: "rgb(96, 112, 128)" },
-        headerTintColor: "white",
-    }} />
-  </Stack.Navigator>
-</NavigationContainer>
-
-    )
-    
+    return (
+        <NavigationContainer independent={true}>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login} options={{
+                    headerTintColor: "white",
+                    headerStyle: { backgroundColor: "rgb(96, 112, 128)" },
+                }} />
+                <Stack.Screen name="Home" component={HomeScreen} options={{
+                    headerStyle: { backgroundColor: "rgb(96, 112, 128)" },
+                    headerTintColor: "white",
+                }} />
+                <Stack.Screen name="Rejestracja" component={Rejestracja} options={{
+                    headerStyle: { backgroundColor: "rgb(96, 112, 128)" },
+                    headerTintColor: "white",
+                }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
